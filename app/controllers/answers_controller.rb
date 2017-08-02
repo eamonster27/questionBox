dfc71ffac5d1929e32f7789c4fe54b5832869dd5
EamonBachari <eamonbachari@gmail.com>
2017-08-02 11:42:55 -0500

    question and answer forms created.

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,2 +1,21 @@
 class AnswersController < ApplicationController
+  
+  def create
+    @qustion = Question.find(params[:question_id])
+    @answer = @qustion.answers.create(answer_params)
+    redirect_to question_path(@qustion)
+  end
+
+  def Destroy
+    @qustion = Question.find(params[:question_id])
+    @answer = @qustion.answers.find(params[:id])
+    @answer.Destroy
+    redirect_to question_path(@qustion)
+  end
+
+  private
+    def answer_params
+      params.require(:answer).permit(:answerer, :content)
+    end
+
 end
--- db/migrate/20170802155236_create_answers.rb
+++ db/migrate/20170802155236_create_answers.rb
@@ -1,7 +1,11 @@
 class CreateAnswers < ActiveRecord::Migration[5.1]
   def change
     create_table :answers do |t|
-
+      t.string :answerer
+      t.text :content
+      t.references :question, foreign_key: true
+      t.references :user, foreign_key: true
+      
       t.timestamps
     end
   end
